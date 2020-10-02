81693e1ff3e9b1d5ccd74e40efa63620db0c23de
Chris on Code <hey@chrisoncode.io>
2020-10-02 14:53:37 -0700

    everything is good!

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ import { getMessages } from "./api-stuff";
 import "./App.css";
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    getMessages().then(setMessages);
+  }, []);
+
+  function addMessage(message) {
+    setMessages([message, ...messages]);
+  }
+
   return (
     <div className="app min-h-screen bg-center">
       <div className="max-w-xl mx-auto px-10 py-20">
         <Header />
+
+        {/* show a form to create a message */}
+        <MessageForm addMessage={addMessage} />
+
+        {/* show all messages */}
+        <Messages messages={messages} />
       </div>
     </div>
   );
